ba9825b4bd18770f8be76bb2b337d2edf885c1ba
Carl Leviss <clmayhuap@gmail.com>
2022-03-03 21:25:01 -0500

    primera version mayhuatravel

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const ham = document.querySelector('.nav__ham');
+const enlaces = document.querySelector('.nav__enlaces-menu');
+const titulo = document.querySelector('.content-tittle')
+ham.addEventListener('click', () => {
+    titulo.classList.toggle('activado');
+    enlaces.classList.toggle('activado');
+});
