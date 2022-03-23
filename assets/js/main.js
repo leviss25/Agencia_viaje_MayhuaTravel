851898ba4dfc9e7673fc5e3a6b16b9c5ab067515
Carl Leviss <clmayhuap@gmail.com>
2022-03-23 03:25:49 -0500

    Se mejoró el nav con animaciones y seudocodigos, además que ahora se tiene un menu hamburguesa en responsive :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
-// const ham = document.querySelector('.nav__ham');
-// const enlaces = document.querySelector('.nav__enlaces-menu');
-// const titulo = document.querySelector('.content-tittle')
-// ham.addEventListener('click', () => {
-//     titulo.classList.toggle('activado');
-//     enlaces.classList.toggle('activado');
-// });
+/********PARA ACTIVAR LA LISTA DESPLEGABLE MOVIL Y ANIMARLO*****/
+const ham = document.querySelector('.nav__ham');
+const enlaces = document.querySelector('.nav__enlaces-menu');
+const barras = document.querySelectorAll('.nav__ham span');
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+});
+/*** PARA QUE SE ACTIVE CON EL SCROLL****/
+window.addEventListener("scroll", function(){
+    var header = document.querySelector(".contenedor-navbar");
+    header.classList.toggle("abajo",window.scrollY>0);
+})
