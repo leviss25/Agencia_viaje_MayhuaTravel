9533beddf0982467f8093189ea67f34fd9163ff2
Carl Leviss <clmayhuap@gmail.com>
2022-03-30 00:48:07 -0500

    Se reprocesó todo el codigo de css desde scss SASS de todo el sitio web, además se mejoró el menu responsive mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,14 @@
 const ham = document.querySelector('.nav__ham');
 const enlaces = document.querySelector('.nav__enlaces-menu');
 const barras = document.querySelectorAll('.nav__ham span');
+const navbarcont = document.querySelector('.header__contenedor-navbar');
 ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
     barras.forEach(child => {child.classList.toggle('animado')});
+    navbarcont.classList.toggle('activado');
 });
 /*** PARA QUE SE ACTIVE CON EL SCROLL****/
 window.addEventListener("scroll", function(){
-    var header = document.querySelector(".contenedor-navbar");
+    var header = document.querySelector(".header__contenedor-navbar");
     header.classList.toggle("abajo",window.scrollY>0);
 })
